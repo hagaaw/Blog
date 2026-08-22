eec24bbb489c2a1716ecce69e38a974a5e5f333b
Ilya Yahnis <ilavoroev@gmail.com>
2026-08-22 23:33:24 +0300

    add necessary classes

---
+++ src/main/java/com/example/blog/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.example.blog.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class AppController {
+    @GetMapping
+    public String getApp(){
+        return "app-page";
+    }
+}
