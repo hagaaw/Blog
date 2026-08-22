1f69df6a2c5ca929478454564af60b960660bbec
Ilya Yahnis <ilavoroev@gmail.com>
2026-08-22 23:47:33 +0300

    add getters and setters in user

--- src/main/java/com/example/blog/entity/UserEntity.java
+++ src/main/java/com/example/blog/entity/UserEntity.java
@@ -7,5 +7,43 @@ import jakarta.persistence.*;
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name="role", nullable = false)
+    private UserRole role;
+
+    public UserEntity(){}
+
+    public UserEntity(String login, UserRole role) {
+        this.login = login;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
